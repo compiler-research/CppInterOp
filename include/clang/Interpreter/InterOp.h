1f0cc28ef1259ea4793ec8bc368e9937e2276b49
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-07-14 11:03:01 +0200

    Implement ExistsFunctionTemplate

--- include/clang/Interpreter/InterOp.h
+++ include/clang/Interpreter/InterOp.h
@@ -81,6 +81,9 @@ namespace InterOp {
                                    bool show_formal_args = false);
 
   bool IsTemplatedFunction(TCppFunction_t func);
+
+  bool ExistsFunctionTemplate(TCppSema_t sema, const std::string &name,
+                              TCppScope_t parent = 0);
 } // end namespace InterOp
 
 } // end namespace cling
