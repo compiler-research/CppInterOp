cc49c7fdb143be9eb1670c8a45dd6130e0bf5368
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-12-01 10:47:12 +0100

    Remove extra local vars in TypeReflectionTest

--- unittests/CppInterOp/TypeReflectionTest.cpp
+++ unittests/CppInterOp/TypeReflectionTest.cpp
@@ -31,8 +31,8 @@ TEST(TypeReflectionTest, GetTypeAsString) {
 }
 
 TEST(TypeReflectionTest, GetSizeOfType) {
-  std::vector<Decl *> Decls, SubDecls0, SubDecls1;
-  std::string code = R"(
+  std::vector<Decl *> Decls;
+  std::string code =  R"(
     struct S {
       int a;
       double b;
@@ -54,7 +54,7 @@ TEST(TypeReflectionTest, GetSizeOfType) {
 }
 
 TEST(TypeReflectionTest, GetCanonicalType) {
-  std::vector<Decl *> Decls, SubDecls0, SubDecls1;
+  std::vector<Decl *> Decls;
   std::string code =  R"(
     typedef int I;
     typedef double D;
