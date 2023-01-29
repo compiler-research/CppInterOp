vgvassilev/cling

Implement IsEnum

unittests/libInterOp/ScopeReflectionTest.cpp
@@ -75,6 +75,12 @@ bool IsAbstract(TCppType_t klass)
 
     return false;
 }
+
+bool IsEnum(TCppScope_t handle)
+{
+    auto *D = (clang::Decl *)handle;
+    return llvm::isa_and_nonnull<clang::EnumDecl>(D);
+}
 }
 
 // This function isn't referenced outside its translation unit, but it
@@ -235,4 +241,23 @@ TEST(ScopeReflectionTest, IsAbstract) {
   GetAllTopLevelDecls(code, Decls);
   EXPECT_FALSE(libInterOp::IsAbstract(Decls[0]));
   EXPECT_TRUE(libInterOp::IsAbstract(Decls[1]));
+}
+
+TEST(ScopeReflectionTest, IsEnum) {
+  std::vector<Decl *> Decls;
+  std::string code = R"(
+    enum Switch {
+      OFF,
+      ON
+    };
+
+    Switch s = Switch::OFF;
+
+    int i = Switch::ON;
+  )";
+
+  GetAllTopLevelDecls(code, Decls);
+  EXPECT_TRUE(libInterOp::IsEnum(Decls[0]));
+  EXPECT_FALSE(libInterOp::IsEnum(Decls[1]));
+  EXPECT_FALSE(libInterOp::IsEnum(Decls[2]));
 }

compiler-research/CppInterOp

unittests/InterOp/ScopeReflectionTest.cpp
@@ -75,6 +75,12 @@ bool IsAbstract(TCppType_t klass)
 
     return false;
 }
+
+bool IsEnum(TCppScope_t handle)
+{
+    auto *D = (clang::Decl *)handle;
+    return llvm::isa_and_nonnull<clang::EnumDecl>(D);
+}
 }
 
 // This function isn't referenced outside its translation unit, but it
@@ -235,4 +241,23 @@ TEST(ScopeReflectionTest, IsAbstract) {
   GetAllTopLevelDecls(code, Decls);
   EXPECT_FALSE(InterOp::IsAbstract(Decls[0]));
   EXPECT_TRUE(InterOp::IsAbstract(Decls[1]));
+}
+
+TEST(ScopeReflectionTest, IsEnum) {
+  std::vector<Decl *> Decls;
+  std::string code = R"(
+    enum Switch {
+      OFF,
+      ON
+    };
+
+    Switch s = Switch::OFF;
+
+    int i = Switch::ON;
+  )";
+
+  GetAllTopLevelDecls(code, Decls);
+  EXPECT_TRUE(InterOp::IsEnum(Decls[0]));
+  EXPECT_FALSE(InterOp::IsEnum(Decls[1]));
+  EXPECT_FALSE(InterOp::IsEnum(Decls[2]));
 }