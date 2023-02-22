be5cba1d3bba9dad4869d29058ecf2d4e62989f1
Baidyanath Kundu <kundubaidya99@gmail.com>
2023-02-22 12:22:57 +0100

    Add test for GetDimensions

--- unittests/InterOp/TypeReflectionTest.cpp
+++ unittests/InterOp/TypeReflectionTest.cpp
@@ -154,3 +154,30 @@ TEST(TypeReflectionTest, DISABLED_IsSubType) {
   // EXPECT_FALSE(InterOp::IsSubType(type_C, type_A));
 }
 
+TEST(TypeReflectionTest, DISABLED_GetDimensions) {
+  std::vector<Decl *> Decls;
+
+  std::string code = R"(
+      int a;
+      int b[1];
+      int c[1][2];
+      int d[1][2][3];
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+
+  auto test_get_dimensions = [](Decl *D, const std::vector<std::size_t> &truth_dims) {
+    // std::vector<TCppIndex_t> dims = InterOp::GetDimensions(InterOp::GetVariableType(D));
+    // EXPECT_EQ(dims.size(), truth_dims.size());
+
+    // for (unsigned i = 0; i < truth_dims.size() && i < dims.size(); i++) {
+    //   EXPECT_EQ(dims[i], truth_dims[i]);
+    // }
+  };
+  
+  test_get_dimensions(Decls[0], {});
+  test_get_dimensions(Decls[1], {1});
+  test_get_dimensions(Decls[2], {1, 2});
+  test_get_dimensions(Decls[3], {1, 2, 3});
+}
+
