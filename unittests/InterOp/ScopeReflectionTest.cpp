05700fb9df0ba80092bca1d5ec73e0d577882cfb
Vassil Vassilev <v.g.vassilev@gmail.com>
2023-04-15 11:43:51 +0000

    Add basic support for instantiating templates with non-template type parameters.

--- unittests/InterOp/ScopeReflectionTest.cpp
+++ unittests/InterOp/ScopeReflectionTest.cpp
@@ -643,12 +643,15 @@ TEST(ScopeReflectionTest, InstantiateClassTemplate) {
     public:
       T m_t;
     };
+
+    template<typename T, unsigned N>
+    class C2{};
   )";
 
   GetAllTopLevelDecls(code, Decls);
   ASTContext &C = Interp->getCI()->getASTContext();
 
-  std::vector<InterOp::TCppType_t> args1 = {C.IntTy.getAsOpaquePtr()};
+  std::vector<InterOp::TemplateArgInfo> args1 = {C.IntTy.getAsOpaquePtr()};
   auto Instance1 = InterOp::InstantiateClassTemplate(Interp.get(), Decls[0],
                                                     args1.data(),
                                                     /*type_size*/args1.size());
@@ -668,7 +671,7 @@ TEST(ScopeReflectionTest, InstantiateClassTemplate) {
   TemplateArgument TA2 = CTSD2->getTemplateArgs().get(0);
   EXPECT_TRUE(TA2.getAsType()->isIntegerType());
 
-  std::vector<InterOp::TCppType_t> args3 = {C.IntTy.getAsOpaquePtr()};
+  std::vector<InterOp::TemplateArgInfo> args3 = {C.IntTy.getAsOpaquePtr()};
   auto Instance3 = InterOp::InstantiateClassTemplate(Interp.get(), Decls[2],
                                                      args3.data(),
                                                      /*type_size*/args3.size());
@@ -684,6 +687,20 @@ TEST(ScopeReflectionTest, InstantiateClassTemplate) {
   auto Inst3_methods = InterOp::GetClassMethods(S, Instance3);
   EXPECT_EQ(InterOp::GetFunctionSignature(Inst3_methods[0]),
             "C1<int>::C1(const C0<int> &val)");
+
+  std::vector<InterOp::TemplateArgInfo> args4 = {C.IntTy.getAsOpaquePtr(),
+                                               {C.IntTy.getAsOpaquePtr(), "3"}};
+  auto Instance4 = InterOp::InstantiateClassTemplate(Interp.get(), Decls[3],
+                                                     args4.data(),
+                                                     /*type_size*/args4.size());
+
+  EXPECT_TRUE(isa<ClassTemplateSpecializationDecl>((Decl*)Instance4));
+  auto *CTSD4 = static_cast<ClassTemplateSpecializationDecl*>(Instance4);
+  EXPECT_TRUE(CTSD4->hasDefinition());
+  TemplateArgument TA4_0 = CTSD4->getTemplateArgs().get(0);
+  TemplateArgument TA4_1 = CTSD4->getTemplateArgs().get(1);
+  EXPECT_TRUE(TA4_0.getAsType()->isIntegerType());
+  EXPECT_TRUE(TA4_1.getAsIntegral() == 3);
 }
 
 TEST(ScopeReflectionTest, IncludeVector) {
