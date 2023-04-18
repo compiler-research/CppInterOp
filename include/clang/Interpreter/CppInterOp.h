70e4b0b201b62a3e1e9d9f1c1d036e442ff3686a
Alexander Penev <7923188+alexander-penev@users.noreply.github.com>
2023-04-18 16:21:39 +0300

    Fix: remove unused and unimplemented function

--- include/clang/Interpreter/CppInterOp.h
+++ include/clang/Interpreter/CppInterOp.h
@@ -115,9 +115,6 @@ namespace Cpp {
 
   TCppType_t GetFunctionArgType(TCppFunction_t func, TCppIndex_t iarg);
 
-  std::string GetFunctionPrototype(TCppFunction_t func,
-                                   bool show_formal_args = false);
-
   /// Returns a stringified version of a given function signature in the form:
   /// void N::f(int i, double d, long l = 0, char ch = 'a').
   std::string GetFunctionSignature(TCppFunction_t func);
