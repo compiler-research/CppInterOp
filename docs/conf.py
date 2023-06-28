e087953608bd7679b89bfc4807bb3e39bfe421fc
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2023-06-28 23:30:18 +0530

    Update changes for documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -25,15 +25,33 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
 
-INTEROP_ROOT = '..'
+html_theme_options = {
+    "github_user": "compiler-research",
+    "github_repo": "CppInterOp",
+    "github_banner": True,
+    "fixed_sidebar": True,
+}
 
-# html_extra_path = [INTEROP_ROOT + '/build/docs/doxygen/html']
+highlight_language = "C++"
+
+todo_include_todos = True
+
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+# Add latex physics package
+mathjax3_config = {
+    "loader": {"load": ["[tex]/physics"]},
+    "tex": {"packages": {"[+]": ["physics"]}},
+}
+
+import os
+INTEROP_ROOT = os.path.abspath('..')
+html_extra_path = [INTEROP_ROOT + '/build/docs/']
 
 import subprocess
-command = 'mkdir {0}/build; cd {0}/build; cmake ../ -DClang_DIR=/usr/lib/llvm-14/lib/cmake/clang\
-         -DLLVM_DIR=/usr/lib/llvm-14 -DINTEROP_ENABLE_DOXYGEN=ON\
+command = 'mkdir {0}/build; cd {0}/build; cmake ../ -DClang_DIR=/usr/lib/llvm-13/build/lib/cmake/clang\
+         -DLLVM_DIR=/usr/lib/llvm-13/build/lib/cmake/llvm -DINTEROP_ENABLE_DOXYGEN=ON\
          -DINTEROP_INCLUDE_DOCS=ON'.format(INTEROP_ROOT)
 subprocess.call(command, shell=True)
 subprocess.call('doxygen {0}/build/docs/doxygen.cfg'.format(INTEROP_ROOT), shell=True)
