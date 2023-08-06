vgvassilev/cling

Implement GetClassMethods

unittests/libInterOp/FunctionReflectionTest.cpp
@@ -1,0 +1,35 @@
+#include "Utils.h"
+
+#include "cling/Interpreter/InterOp.h"
+
+#include "gtest/gtest.h"
+
+
+TEST(FunctionReflectionTest, GetClassMethods) {
+  std::vector<Decl*> Decls;
+  std::string code = R"(
+    class A {
+    public:
+      int f1(int a, int b) { return a + b; }
+      const A *f2() const { return this; }
+    private:
+      int f3() { return 0; }
+      void f4() {}
+    protected:
+      int f5(int i) { return i; }
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  auto methods = InterOp::GetClassMethods(Decls[0]);
+
+  auto get_method_name = [](InterOp::TCppFunction_t method) {
+    return InterOp::GetCompleteName(method);
+  };
+
+  EXPECT_EQ(get_method_name(methods[0]), "A::f1");
+  EXPECT_EQ(get_method_name(methods[1]), "A::f2");
+  EXPECT_EQ(get_method_name(methods[2]), "A::f3");
+  EXPECT_EQ(get_method_name(methods[3]), "A::f4");
+  EXPECT_EQ(get_method_name(methods[4]), "A::f5");
+}

compiler-research/CppInterOp

unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -1,0 +1,35 @@
+#include "Utils.h"
+
+#include "clang/Interpreter/CppInterOp.h"
+
+#include "gtest/gtest.h"
+
+
+TEST(FunctionReflectionTest, GetClassMethods) {
+  std::vector<Decl*> Decls;
+  std::string code = R"(
+    class A {
+    public:
+      int f1(int a, int b) { return a + b; }
+      const A *f2() const { return this; }
+    private:
+      int f3() { return 0; }
+      void f4() {}
+    protected:
+      int f5(int i) { return i; }
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  auto methods = Cpp::GetClassMethods(Decls[0]);
+
+  auto get_method_name = [](Cpp::TCppFunction_t method) {
+    return Cpp::GetCompleteName(method);
+  };
+
+  EXPECT_EQ(get_method_name(methods[0]), "A::f1");
+  EXPECT_EQ(get_method_name(methods[1]), "A::f2");
+  EXPECT_EQ(get_method_name(methods[2]), "A::f3");
+  EXPECT_EQ(get_method_name(methods[3]), "A::f4");
+  EXPECT_EQ(get_method_name(methods[4]), "A::f5");
+}