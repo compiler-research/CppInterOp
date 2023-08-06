vgvassilev/cling

Implement IsPrivateMethod

unittests/libInterOp/FunctionReflectionTest.cpp
@@ -357,3 +357,28 @@ TEST(FunctionReflectionTest, IsProtectedMethod) {
   EXPECT_FALSE(InterOp::IsProtectedMethod(SubDecls[6]));
   EXPECT_TRUE(InterOp::IsProtectedMethod(SubDecls[8]));
 }
+
+TEST(FunctionReflectionTest, IsPrivateMethod) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      C() {}
+      void pub_f() {}
+      ~C() {}
+    private:
+      void pri_f() {}
+    protected:
+      void pro_f() {}
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_FALSE(InterOp::IsPrivateMethod(SubDecls[2]));
+  EXPECT_FALSE(InterOp::IsPrivateMethod(SubDecls[3]));
+  EXPECT_FALSE(InterOp::IsPrivateMethod(SubDecls[4]));
+  EXPECT_TRUE(InterOp::IsPrivateMethod(SubDecls[6]));
+  EXPECT_FALSE(InterOp::IsPrivateMethod(SubDecls[8]));
+}

compiler-research/CppInterOp

unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -365,3 +365,28 @@ TEST(FunctionReflectionTest, IsProtectedMethod) {
   EXPECT_FALSE(Cpp::IsProtectedMethod(SubDecls[6]));
   EXPECT_TRUE(Cpp::IsProtectedMethod(SubDecls[8]));
 }
+
+TEST(FunctionReflectionTest, IsPrivateMethod) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      C() {}
+      void pub_f() {}
+      ~C() {}
+    private:
+      void pri_f() {}
+    protected:
+      void pro_f() {}
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_FALSE(Cpp::IsPrivateMethod(SubDecls[2]));
+  EXPECT_FALSE(Cpp::IsPrivateMethod(SubDecls[3]));
+  EXPECT_FALSE(Cpp::IsPrivateMethod(SubDecls[4]));
+  EXPECT_TRUE(Cpp::IsPrivateMethod(SubDecls[6]));
+  EXPECT_FALSE(Cpp::IsPrivateMethod(SubDecls[8]));
+}