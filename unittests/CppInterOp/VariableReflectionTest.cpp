vgvassilev/cling

Change GetVariableTypeAsString to GetVariableType

unittests/libInterOp/VariableReflectionTest.cpp
@@ -31,7 +31,7 @@ TEST(VariableReflectionTest, GetDatamembers) {
   EXPECT_EQ(datamembers.size(), 3);
 }
 
-TEST(VariableReflectionTest, GetVariableTypeAsString) {
+TEST(VariableReflectionTest, GetVariableType) {
   std::vector<Decl*> Decls;
   std::string code = R"(
     class C {};
@@ -45,16 +45,18 @@ TEST(VariableReflectionTest, GetVariableTypeAsString) {
     C *d;
     E<int> e;
     E<int> *f;
+    int g[4];
     )";
 
   GetAllTopLevelDecls(code, Decls);
 
-  EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[2]), "int");
-  EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[3]), "char");
-  EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[4]), "class C");
-  EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[5]), "class C *");
-  EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[6]), "E<int>");
-  EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[7]), "E<int> *");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[2])), "int");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[3])), "char");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[4])), "class C");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[5])), "class C *");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[6])), "E<int>");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[7])), "E<int> *");
+  EXPECT_EQ(InterOp::GetTypeAsString(InterOp::GetVariableType(Decls[8])), "int [4]");
 }
 
 TEST(VariableReflectionTest, GetVariableOffset) {

compiler-research/CppInterOp

unittests/CppInterOp/VariableReflectionTest.cpp
@@ -31,7 +31,7 @@ TEST(VariableReflectionTest, GetDatamembers) {
   EXPECT_EQ(datamembers.size(), 3);
 }
 
-TEST(VariableReflectionTest, GetVariableTypeAsString) {
+TEST(VariableReflectionTest, GetVariableType) {
   std::vector<Decl*> Decls;
   std::string code = R"(
     class C {};
@@ -45,16 +45,18 @@ TEST(VariableReflectionTest, GetVariableTypeAsString) {
     C *d;
     E<int> e;
     E<int> *f;
+    int g[4];
     )";
 
   GetAllTopLevelDecls(code, Decls);
 
-  EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[2]), "int");
-  EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[3]), "char");
-  EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[4]), "class C");
-  EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[5]), "class C *");
-  EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[6]), "E<int>");
-  EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[7]), "E<int> *");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[2])), "int");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[3])), "char");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[4])), "class C");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[5])), "class C *");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[6])), "E<int>");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[7])), "E<int> *");
+  EXPECT_EQ(Cpp::GetTypeAsString(Cpp::GetVariableType(Decls[8])), "int [4]");
 }
 
 TEST(VariableReflectionTest, GetVariableOffset) {