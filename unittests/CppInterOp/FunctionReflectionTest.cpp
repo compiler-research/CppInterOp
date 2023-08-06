vgvassilev/cling

Implement GetFunctionRequiredArgs

unittests/libInterOp/FunctionReflectionTest.cpp
@@ -133,3 +133,19 @@ TEST(FunctionReflectionTest, GetFunctionNumArgs) {
   EXPECT_EQ(InterOp::GetFunctionNumArgs(Decls[2]), (size_t) 4);
   EXPECT_EQ(InterOp::GetFunctionNumArgs(Decls[3]), (size_t) 4);
 }
+
+TEST(FunctionReflectionTest, GetFunctionRequiredArgs) {
+  std::vector<Decl*> Decls, SubDecls;
+  std::string code = R"(
+    void f1() {}
+    void f2(int i, double d, long l, char ch) {}
+    void f3(int i, double d, long l = 0, char ch = 'a') {}
+    void f4(int i = 0, double d = 0.0, long l = 0, char ch = 'a') {}
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  EXPECT_EQ(InterOp::GetFunctionRequiredArgs(Decls[0]), (size_t) 0);
+  EXPECT_EQ(InterOp::GetFunctionRequiredArgs(Decls[1]), (size_t) 4);
+  EXPECT_EQ(InterOp::GetFunctionRequiredArgs(Decls[2]), (size_t) 2);
+  EXPECT_EQ(InterOp::GetFunctionRequiredArgs(Decls[3]), (size_t) 0);
+}

compiler-research/CppInterOp

unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -135,3 +135,19 @@ TEST(FunctionReflectionTest, GetFunctionNumArgs) {
   EXPECT_EQ(Cpp::GetFunctionNumArgs(Decls[2]), (size_t) 4);
   EXPECT_EQ(Cpp::GetFunctionNumArgs(Decls[3]), (size_t) 4);
 }
+
+TEST(FunctionReflectionTest, GetFunctionRequiredArgs) {
+  std::vector<Decl*> Decls, SubDecls;
+  std::string code = R"(
+    void f1() {}
+    void f2(int i, double d, long l, char ch) {}
+    void f3(int i, double d, long l = 0, char ch = 'a') {}
+    void f4(int i = 0, double d = 0.0, long l = 0, char ch = 'a') {}
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  EXPECT_EQ(Cpp::GetFunctionRequiredArgs(Decls[0]), (size_t) 0);
+  EXPECT_EQ(Cpp::GetFunctionRequiredArgs(Decls[1]), (size_t) 4);
+  EXPECT_EQ(Cpp::GetFunctionRequiredArgs(Decls[2]), (size_t) 2);
+  EXPECT_EQ(Cpp::GetFunctionRequiredArgs(Decls[3]), (size_t) 0);
+}