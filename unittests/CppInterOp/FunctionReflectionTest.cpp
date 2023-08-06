vgvassilev/cling

Implement IsConstructor

unittests/libInterOp/FunctionReflectionTest.cpp
@@ -382,3 +382,28 @@ TEST(FunctionReflectionTest, IsPrivateMethod) {
   EXPECT_TRUE(InterOp::IsPrivateMethod(SubDecls[6]));
   EXPECT_FALSE(InterOp::IsPrivateMethod(SubDecls[8]));
 }
+
+TEST(FunctionReflectionTest, IsConstructor) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      C() {}
+      void pub_f() {}
+      ~C() {}
+    private:
+      void pri_f() {}
+    protected:
+      void pro_f() {}
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_TRUE(InterOp::IsConstructor(SubDecls[2]));
+  EXPECT_FALSE(InterOp::IsConstructor(SubDecls[3]));
+  EXPECT_FALSE(InterOp::IsConstructor(SubDecls[4]));
+  EXPECT_FALSE(InterOp::IsConstructor(SubDecls[6]));
+  EXPECT_FALSE(InterOp::IsConstructor(SubDecls[8]));
+}

compiler-research/CppInterOp

unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -390,3 +390,28 @@ TEST(FunctionReflectionTest, IsPrivateMethod) {
   EXPECT_TRUE(Cpp::IsPrivateMethod(SubDecls[6]));
   EXPECT_FALSE(Cpp::IsPrivateMethod(SubDecls[8]));
 }
+
+TEST(FunctionReflectionTest, IsConstructor) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      C() {}
+      void pub_f() {}
+      ~C() {}
+    private:
+      void pri_f() {}
+    protected:
+      void pro_f() {}
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_TRUE(Cpp::IsConstructor(SubDecls[2]));
+  EXPECT_FALSE(Cpp::IsConstructor(SubDecls[3]));
+  EXPECT_FALSE(Cpp::IsConstructor(SubDecls[4]));
+  EXPECT_FALSE(Cpp::IsConstructor(SubDecls[6]));
+  EXPECT_FALSE(Cpp::IsConstructor(SubDecls[8]));
+}