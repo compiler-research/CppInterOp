vgvassilev/cling

Add support for global scope in GetName

unittests/libInterOp/ScopeReflectionTest.cpp
@@ -224,6 +224,7 @@ TEST(ScopeReflectionTest, GetGlobalScope) {
   Sema *S = &Interp->getCI()->getSema();
 
   EXPECT_EQ(InterOp::GetCompleteName(InterOp::GetGlobalScope(S)), "");
+  EXPECT_EQ(InterOp::GetName(InterOp::GetGlobalScope(S)), "");
 }
 
 TEST(ScopeReflectionTest, GetScope) {

compiler-research/CppInterOp

unittests/CppInterOp/ScopeReflectionTest.cpp
@@ -257,6 +257,7 @@ TEST(ScopeReflectionTest, GetGlobalScope) {
   Sema *S = &Interp->getCI()->getSema();
 
   EXPECT_EQ(Cpp::GetCompleteName(Cpp::GetGlobalScope(S)), "");
+  EXPECT_EQ(Cpp::GetName(Cpp::GetGlobalScope(S)), "");
 }
 
 TEST(ScopeReflectionTest, GetScope) {