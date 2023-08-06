vgvassilev/cling

Implement GetFunctionReturnTypeAsString

unittests/libInterOp/FunctionReflectionTest.cpp
@@ -80,3 +80,40 @@ TEST(FunctionReflectionTest, GetFunctionsUsingName) {
   test_get_funcs_using_name(Decls[0], "f3", 1);
   test_get_funcs_using_name(Decls[1], "f4", 2);
 }
+
+TEST(FunctionReflectionTest, GetFunctionReturnTypeAsString) {
+  std::vector<Decl*> Decls, SubDecls;
+  std::string code = R"(
+    namespace N { class C {}; }
+    enum Switch { OFF, ON };
+
+    class A {
+      A (int i) { i++; }
+      int f () { return 0; }
+    };
+
+
+    void f1() {}
+    double f2() { return 0.2; }
+    Switch f3() { return ON; }
+    N::C f4() { return N::C(); }
+    N::C *f5() { return new N::C(); }
+    const N::C f6() { return N::C(); }
+    volatile N::C f7() { return N::C(); }
+    const volatile N::C f8() { return N::C(); }
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[2], SubDecls);
+
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[3]), "void");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[4]), "double");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[5]), "enum Switch");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[6]), "N::C");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[7]), "N::C *");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[8]), "const N::C");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[9]), "volatile N::C");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(Decls[10]), "const volatile N::C");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(SubDecls[1]), "void");
+  EXPECT_EQ(InterOp::GetFunctionReturnTypeAsString(SubDecls[2]), "int");
+}

compiler-research/CppInterOp

unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -81,3 +81,41 @@ TEST(FunctionReflectionTest, GetFunctionsUsingName) {
   test_get_funcs_using_name(Decls[0], "f3", 1);
   test_get_funcs_using_name(Decls[1], "f4", 2);
 }
+
+TEST(FunctionReflectionTest, GetFunctionReturnTypeAsString) {
+  std::vector<Decl*> Decls, SubDecls;
+  std::string code = R"(
+    namespace N { class C {}; }
+    enum Switch { OFF, ON };
+
+    class A {
+      A (int i) { i++; }
+      int f () { return 0; }
+    };
+
+
+    void f1() {}
+    double f2() { return 0.2; }
+    Switch f3() { return ON; }
+    N::C f4() { return N::C(); }
+    N::C *f5() { return new N::C(); }
+    const N::C f6() { return N::C(); }
+    volatile N::C f7() { return N::C(); }
+    const volatile N::C f8() { return N::C(); }
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[2], SubDecls);
+
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[3]), "void");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[4]), "double");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[5]), "enum Switch");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[6]), "N::C");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[7]), "N::C *");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[8]), "const N::C");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[9]), "volatile N::C");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(Decls[10]),
+            "const volatile N::C");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(SubDecls[1]), "void");
+  EXPECT_EQ(Cpp::GetFunctionReturnTypeAsString(SubDecls[2]), "int");
+}