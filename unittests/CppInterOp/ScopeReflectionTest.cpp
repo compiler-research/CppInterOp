vgvassilev/cling

Initial implementation of SizeOf

unittests/libInterOp/ScopeReflectionTest.cpp
@@ -4,6 +4,7 @@
 
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclCXX.h"
+#include "clang/AST/RecordLayout.h"
 #include "clang/Basic/Version.h"
 #include "clang/Config/config.h"
 
@@ -33,15 +34,20 @@ bool IsComplete(TCppScope_t scope) {
   // Everything else is considered complete.
   return true;
 }
-}
 
-static void GetTopLevelDecls(cling::Transaction &T, std::vector<Decl*>& Decls) {
-  for (auto DCI = T.decls_begin(), E = T.decls_end(); DCI != E; ++DCI) {
-    if (DCI->m_Call != cling::Transaction::kCCIHandleTopLevelDecl)
-      continue;
-    assert(DCI->m_DGR.isSingleDecl());
-    Decls.push_back(DCI->m_DGR.getSingleDecl());
+size_t SizeOf(TCppScope_t scope) {
+  assert (scope);
+  if (!IsComplete(scope))
+    return 0;
+
+  if (auto *RD = dyn_cast<RecordDecl>(static_cast<Decl*>(scope))) {
+    ASTContext &Context = RD->getASTContext();
+    const ASTRecordLayout &Layout = Context.getASTRecordLayout(RD);
+    return Layout.getSize().getQuantity();
   }
+
+  return 0;
+}
 }
 
 // This function isn't referenced outside its translation unit, but it
@@ -73,7 +79,6 @@ static std::string MakeResourcesPath() {
   return std::string(P.str());
 }
 
-
 static std::unique_ptr<cling::Interpreter> createInterpreter() {
   std::string MainExecutableName =
     llvm::sys::fs::getMainExecutable(nullptr, nullptr);
@@ -82,30 +87,54 @@ static std::unique_ptr<cling::Interpreter> createInterpreter() {
   ClingArgv.insert(ClingArgv.begin(), MainExecutableName.c_str());
   return llvm::make_unique<cling::Interpreter>(ClingArgv.size(), &ClingArgv[0]);
 }
+std:: unique_ptr<cling::Interpreter> Interp;
+static void GetAllTopLevelDecls(const std::string& code, std::vector<Decl*>& Decls) {
+  Interp = createInterpreter();
+  cling::Transaction *T = nullptr;
+  Interp->declare(code, &T);
+  for (auto DCI = T->decls_begin(), E = T->decls_end(); DCI != E; ++DCI) {
+    if (DCI->m_Call != cling::Transaction::kCCIHandleTopLevelDecl)
+      continue;
+    assert(DCI->m_DGR.isSingleDecl());
+    Decls.push_back(DCI->m_DGR.getSingleDecl());
+  }
+}
 
 
 // Check that the CharInfo table has been constructed reasonably.
 TEST(ScopeReflectionTest, IsNamespace) {
-  auto Interp = createInterpreter();
-  cling::Transaction *T = nullptr;
-  Interp->declare("namespace N {} class C{}; int I;", &T);
   std::vector<Decl*> Decls;
-  GetTopLevelDecls(*T, Decls);
+  GetAllTopLevelDecls("namespace N {} class C{}; int I;", Decls);
   EXPECT_TRUE(libInterOp::IsNamespace(Decls[0]));
   EXPECT_FALSE(libInterOp::IsNamespace(Decls[1]));
   EXPECT_FALSE(libInterOp::IsNamespace(Decls[2]));
 }
 
 TEST(ScopeReflectionTest, IsComplete) {
-  auto Interp = createInterpreter();
-  cling::Transaction *T = nullptr;
-  Interp->declare("namespace N {} class C{}; int I; struct S; enum E : int; union U{};", &T);
   std::vector<Decl*> Decls;
-  GetTopLevelDecls(*T, Decls);
+  GetAllTopLevelDecls("namespace N {} class C{}; int I; struct S; enum E : int; union U{};",
+                      Decls);
   EXPECT_TRUE(libInterOp::IsComplete(Decls[0]));
   EXPECT_TRUE(libInterOp::IsComplete(Decls[1]));
   EXPECT_TRUE(libInterOp::IsComplete(Decls[2]));
   EXPECT_FALSE(libInterOp::IsComplete(Decls[3]));
   EXPECT_FALSE(libInterOp::IsComplete(Decls[4]));
   EXPECT_TRUE(libInterOp::IsComplete(Decls[5]));
+}
+
+TEST(ScopeReflectionTest, SizeOf) {
+  std::vector<Decl*> Decls;
+  std::string code = R"(namespace N {} class C{}; int I; struct S;
+                        enum E : int; union U{}; class Size4{int i;};
+                        struct Size16 {short a; double b;};
+                       )";
+  GetAllTopLevelDecls(code, Decls);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[0]), (size_t)0);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[1]), (size_t)1);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[2]), (size_t)0);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[3]), (size_t)0);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[4]), (size_t)0);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[5]), (size_t)1);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[6]), (size_t)4);
+  EXPECT_EQ(libInterOp::SizeOf(Decls[7]), (size_t)16);
 }

compiler-research/CppInterOp

unittests/CppInterOp/ScopeReflectionTest.cpp
@@ -4,6 +4,7 @@
 
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclCXX.h"
+#include "clang/AST/RecordLayout.h"
 #include "clang/Basic/Version.h"
 #include "clang/Config/config.h"
 
@@ -33,17 +34,21 @@ bool IsComplete(TCppScope_t scope) {
   // Everything else is considered complete.
   return true;
 }
-} // namespace Cpp
 
-static void GetTopLevelDecls(cling::Transaction &T,
-                             std::vector<Decl *> &Decls) {
-  for (auto DCI = T.decls_begin(), E = T.decls_end(); DCI != E; ++DCI) {
-    if (DCI->m_Call != cling::Transaction::kCCIHandleTopLevelDecl)
-      continue;
-    assert(DCI->m_DGR.isSingleDecl());
-    Decls.push_back(DCI->m_DGR.getSingleDecl());
+size_t SizeOf(TCppScope_t scope) {
+  assert(scope);
+  if (!IsComplete(scope))
+    return 0;
+
+  if (auto *RD = dyn_cast<RecordDecl>(static_cast<Decl *>(scope))) {
+    ASTContext &Context = RD->getASTContext();
+    const ASTRecordLayout &Layout = Context.getASTRecordLayout(RD);
+    return Layout.getSize().getQuantity();
   }
+
+  return 0;
 }
+} // namespace Cpp
 
 // This function isn't referenced outside its translation unit, but it
 // can't use the "static" keyword because its address is used for
@@ -82,31 +87,55 @@ static std::unique_ptr<cling::Interpreter> createInterpreter() {
   ClingArgv.insert(ClingArgv.begin(), MainExecutableName.c_str());
   return llvm::make_unique<cling::Interpreter>(ClingArgv.size(), &ClingArgv[0]);
 }
+std::unique_ptr<cling::Interpreter> Interp;
+static void GetAllTopLevelDecls(const std::string &code,
+                                std::vector<Decl *> &Decls) {
+  Interp = createInterpreter();
+  cling::Transaction *T = nullptr;
+  Interp->declare(code, &T);
+  for (auto DCI = T->decls_begin(), E = T->decls_end(); DCI != E; ++DCI) {
+    if (DCI->m_Call != cling::Transaction::kCCIHandleTopLevelDecl)
+      continue;
+    assert(DCI->m_DGR.isSingleDecl());
+    Decls.push_back(DCI->m_DGR.getSingleDecl());
+  }
+}
 
 // Check that the CharInfo table has been constructed reasonably.
 TEST(ScopeReflectionTest, IsNamespace) {
-  auto Interp = createInterpreter();
-  cling::Transaction *T = nullptr;
-  Interp->declare("namespace N {} class C{}; int I;", &T);
   std::vector<Decl*> Decls;
-  GetTopLevelDecls(*T, Decls);
+  GetAllTopLevelDecls("namespace N {} class C{}; int I;", Decls);
   EXPECT_TRUE(Cpp::IsNamespace(Decls[0]));
   EXPECT_FALSE(Cpp::IsNamespace(Decls[1]));
   EXPECT_FALSE(Cpp::IsNamespace(Decls[2]));
 }
 
 TEST(ScopeReflectionTest, IsComplete) {
-  auto Interp = createInterpreter();
-  cling::Transaction *T = nullptr;
-  Interp->declare(
-      "namespace N {} class C{}; int I; struct S; enum E : int; union U{};",
-      &T);
   std::vector<Decl*> Decls;
-  GetTopLevelDecls(*T, Decls);
+  GetAllTopLevelDecls(
+      "namespace N {} class C{}; int I; struct S; enum E : int; union U{};",
+      Decls);
   EXPECT_TRUE(Cpp::IsComplete(Decls[0]));
   EXPECT_TRUE(Cpp::IsComplete(Decls[1]));
   EXPECT_TRUE(Cpp::IsComplete(Decls[2]));
   EXPECT_FALSE(Cpp::IsComplete(Decls[3]));
   EXPECT_FALSE(Cpp::IsComplete(Decls[4]));
   EXPECT_TRUE(Cpp::IsComplete(Decls[5]));
+}
+
+TEST(ScopeReflectionTest, SizeOf) {
+  std::vector<Decl*> Decls;
+  std::string code = R"(namespace N {} class C{}; int I; struct S;
+                        enum E : int; union U{}; class Size4{int i;};
+                        struct Size16 {short a; double b;};
+                       )";
+  GetAllTopLevelDecls(code, Decls);
+  EXPECT_EQ(Cpp::SizeOf(Decls[0]), (size_t)0);
+  EXPECT_EQ(Cpp::SizeOf(Decls[1]), (size_t)1);
+  EXPECT_EQ(Cpp::SizeOf(Decls[2]), (size_t)0);
+  EXPECT_EQ(Cpp::SizeOf(Decls[3]), (size_t)0);
+  EXPECT_EQ(Cpp::SizeOf(Decls[4]), (size_t)0);
+  EXPECT_EQ(Cpp::SizeOf(Decls[5]), (size_t)1);
+  EXPECT_EQ(Cpp::SizeOf(Decls[6]), (size_t)4);
+  EXPECT_EQ(Cpp::SizeOf(Decls[7]), (size_t)16);
 }