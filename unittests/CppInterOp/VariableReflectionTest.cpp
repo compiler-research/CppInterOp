vgvassilev/cling

Implement IsProtectedVariable

unittests/libInterOp/VariableReflectionTest.cpp
@@ -115,3 +115,24 @@ TEST(VariableReflectionTest, IsPublicVariable) {
   EXPECT_FALSE(InterOp::IsPublicVariable(SubDecls[4]));
   EXPECT_FALSE(InterOp::IsPublicVariable(SubDecls[6]));
 }
+
+TEST(VariableReflectionTest, IsProtectedVariable) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      int a;
+    private:
+      int b;
+    protected:
+      int c;
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_FALSE(InterOp::IsProtectedVariable(SubDecls[2]));
+  EXPECT_FALSE(InterOp::IsProtectedVariable(SubDecls[4]));
+  EXPECT_TRUE(InterOp::IsProtectedVariable(SubDecls[6]));
+}

compiler-research/CppInterOp

unittests/CppInterOp/VariableReflectionTest.cpp
@@ -112,3 +112,24 @@ TEST(VariableReflectionTest, IsPublicVariable) {
   EXPECT_FALSE(Cpp::IsPublicVariable(SubDecls[4]));
   EXPECT_FALSE(Cpp::IsPublicVariable(SubDecls[6]));
 }
+
+TEST(VariableReflectionTest, IsProtectedVariable) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      int a;
+    private:
+      int b;
+    protected:
+      int c;
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_FALSE(Cpp::IsProtectedVariable(SubDecls[2]));
+  EXPECT_FALSE(Cpp::IsProtectedVariable(SubDecls[4]));
+  EXPECT_TRUE(Cpp::IsProtectedVariable(SubDecls[6]));
+}