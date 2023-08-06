vgvassilev/cling

Implement IsPublicVariable

unittests/libInterOp/VariableReflectionTest.cpp
@@ -94,3 +94,24 @@ TEST(VariableReflectionTest, GetVariableOffset) {
   EXPECT_EQ(InterOp::GetVariableOffset(Interp.get(), datamembers[3]),
           ((unsigned long) &(c.d)) - ((unsigned long) &(c.a)));
 }
+
+TEST(VariableReflectionTest, IsPublicVariable) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      int a;
+    private:
+      int b;
+    protected:
+      int c;
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_TRUE(InterOp::IsPublicVariable(SubDecls[2]));
+  EXPECT_FALSE(InterOp::IsPublicVariable(SubDecls[4]));
+  EXPECT_FALSE(InterOp::IsPublicVariable(SubDecls[6]));
+}

compiler-research/CppInterOp

unittests/CppInterOp/VariableReflectionTest.cpp
@@ -91,3 +91,24 @@ TEST(VariableReflectionTest, GetVariableOffset) {
   EXPECT_EQ(Cpp::GetVariableOffset(Interp.get(), datamembers[3]),
             ((unsigned long)&(c.d)) - ((unsigned long)&(c.a)));
 }
+
+TEST(VariableReflectionTest, IsPublicVariable) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+    public:
+      int a;
+    private:
+      int b;
+    protected:
+      int c;
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_TRUE(Cpp::IsPublicVariable(SubDecls[2]));
+  EXPECT_FALSE(Cpp::IsPublicVariable(SubDecls[4]));
+  EXPECT_FALSE(Cpp::IsPublicVariable(SubDecls[6]));
+}