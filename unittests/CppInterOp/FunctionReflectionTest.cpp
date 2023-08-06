vgvassilev/cling

Implement GetFunctionSignature

unittests/libInterOp/FunctionReflectionTest.cpp
@@ -149,3 +149,67 @@ TEST(FunctionReflectionTest, GetFunctionRequiredArgs) {
   EXPECT_EQ(InterOp::GetFunctionRequiredArgs(Decls[2]), (size_t) 2);
   EXPECT_EQ(InterOp::GetFunctionRequiredArgs(Decls[3]), (size_t) 0);
 }
+
+TEST(FunctionReflectionTest, GetFunctionSignature) {
+  std::vector<Decl*> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+      void f(int i, double d, long l = 0, char ch = 'a') {}
+    };
+
+    namespace N
+    {
+      void f(int i, double d, long l = 0, char ch = 'a') {}
+    }
+
+    void f1() {}
+    C f2(int i, double d, long l = 0, char ch = 'a') { return C(); }
+    C *f3(int i, double d, long l = 0, char ch = 'a') { return new C(); }
+    void f4(int i = 0, double d = 0.0, long l = 0, char ch = 'a') {}
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], Decls);
+  GetAllSubDecls(Decls[1], Decls);
+
+  auto test_func_sig = [](Decl *D, bool formal_args,
+          size_t max_args, std::string sig) {
+      EXPECT_EQ(InterOp::GetFunctionSignature(D, formal_args, max_args), sig);
+  };
+
+  test_func_sig(Decls[2], false, -1, "void ()"); // f1
+  test_func_sig(Decls[2], true, -1, "void ()"); // f1
+  test_func_sig(Decls[2], true, 3, "void ()"); // f1
+  test_func_sig(Decls[3], false, -1,
+          "class C (int, double, long, char)"); // f2
+  test_func_sig(Decls[3], false, 0,
+          "class C ()"); // f2
+  test_func_sig(Decls[3], true, -1,
+          "class C (int i, double d, long l = 0, char ch = 'a')"); // f2
+  test_func_sig(Decls[3], true, 0,
+          "class C ()"); // f2
+  test_func_sig(Decls[4], false, -1,
+          "class C *(int, double, long, char)"); // f3
+  test_func_sig(Decls[4], false, 5,
+          "class C *(int, double, long, char)"); // f3
+  test_func_sig(Decls[4], true, -1,
+          "class C *(int i, double d, long l = 0, char ch = 'a')"); // f3
+  test_func_sig(Decls[4], true, 5,
+          "class C *(int i, double d, long l = 0, char ch = 'a')"); // f3
+  test_func_sig(Decls[5], false, -1,
+          "void (int, double, long, char)"); // f4
+  test_func_sig(Decls[5], false, 3,
+          "void (int, double, long)"); // f4
+  test_func_sig(Decls[5], true, -1,
+          "void (int i = 0, double d = 0., long l = 0, char ch = 'a')"); // f4
+  test_func_sig(Decls[5], true, 3,
+          "void (int i = 0, double d = 0., long l = 0)"); // f4
+  test_func_sig(Decls[7], false, -1,
+          "void (int, double, long, char)"); // C::f
+  test_func_sig(Decls[7], true, -1,
+          "void (int i, double d, long l = 0, char ch = 'a')"); // C::f
+  test_func_sig(Decls[12], false, -1,
+          "void (int, double, long, char)"); // N::f
+  test_func_sig(Decls[12], true, -1,
+          "void (int i, double d, long l = 0, char ch = 'a')"); // N::f
+}

compiler-research/CppInterOp

unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -151,3 +151,68 @@ TEST(FunctionReflectionTest, GetFunctionRequiredArgs) {
   EXPECT_EQ(Cpp::GetFunctionRequiredArgs(Decls[2]), (size_t) 2);
   EXPECT_EQ(Cpp::GetFunctionRequiredArgs(Decls[3]), (size_t) 0);
 }
+
+TEST(FunctionReflectionTest, GetFunctionSignature) {
+  std::vector<Decl*> Decls, SubDecls;
+  std::string code = R"(
+    class C {
+      void f(int i, double d, long l = 0, char ch = 'a') {}
+    };
+
+    namespace N
+    {
+      void f(int i, double d, long l = 0, char ch = 'a') {}
+    }
+
+    void f1() {}
+    C f2(int i, double d, long l = 0, char ch = 'a') { return C(); }
+    C *f3(int i, double d, long l = 0, char ch = 'a') { return new C(); }
+    void f4(int i = 0, double d = 0.0, long l = 0, char ch = 'a') {}
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], Decls);
+  GetAllSubDecls(Decls[1], Decls);
+
+  auto test_func_sig = [](Decl *D, bool formal_args, size_t max_args,
+                          std::string sig) {
+    EXPECT_EQ(Cpp::GetFunctionSignature(D, formal_args, max_args), sig);
+  };
+
+  test_func_sig(Decls[2], false, -1, "void ()"); // f1
+  test_func_sig(Decls[2], true, -1, "void ()");  // f1
+  test_func_sig(Decls[2], true, 3, "void ()");   // f1
+  test_func_sig(Decls[3], false, -1,
+                "class C (int, double, long, char)"); // f2
+  test_func_sig(Decls[3], false, 0,
+                "class C ()"); // f2
+  test_func_sig(Decls[3], true, -1,
+                "class C (int i, double d, long l = 0, char ch = 'a')"); // f2
+  test_func_sig(Decls[3], true, 0,
+                "class C ()"); // f2
+  test_func_sig(Decls[4], false, -1,
+                "class C *(int, double, long, char)"); // f3
+  test_func_sig(Decls[4], false, 5,
+                "class C *(int, double, long, char)"); // f3
+  test_func_sig(Decls[4], true, -1,
+                "class C *(int i, double d, long l = 0, char ch = 'a')"); // f3
+  test_func_sig(Decls[4], true, 5,
+                "class C *(int i, double d, long l = 0, char ch = 'a')"); // f3
+  test_func_sig(Decls[5], false, -1,
+                "void (int, double, long, char)"); // f4
+  test_func_sig(Decls[5], false, 3,
+                "void (int, double, long)"); // f4
+  test_func_sig(
+      Decls[5], true, -1,
+      "void (int i = 0, double d = 0., long l = 0, char ch = 'a')"); // f4
+  test_func_sig(Decls[5], true, 3,
+                "void (int i = 0, double d = 0., long l = 0)"); // f4
+  test_func_sig(Decls[7], false, -1,
+                "void (int, double, long, char)"); // C::f
+  test_func_sig(Decls[7], true, -1,
+                "void (int i, double d, long l = 0, char ch = 'a')"); // C::f
+  test_func_sig(Decls[12], false, -1,
+                "void (int, double, long, char)"); // N::f
+  test_func_sig(Decls[12], true, -1,
+                "void (int i, double d, long l = 0, char ch = 'a')"); // N::f
+}