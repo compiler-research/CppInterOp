vgvassilev/cling

Implement IsConstVariable

unittests/libInterOp/VariableReflectionTest.cpp
@@ -173,3 +173,19 @@ TEST(VariableReflectionTest, IsStaticVariable) {
   EXPECT_FALSE(InterOp::IsStaticVariable(SubDecls[1]));
   EXPECT_TRUE(InterOp::IsStaticVariable(SubDecls[2]));
 }
+
+TEST(VariableReflectionTest, IsConstVariable) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code =  R"(
+    class C {
+      int a;
+      const int b = 2;
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_FALSE(InterOp::IsConstVariable(SubDecls[1]));
+  EXPECT_TRUE(InterOp::IsConstVariable(SubDecls[2]));
+}

compiler-research/CppInterOp

unittests/CppInterOp/VariableReflectionTest.cpp
@@ -170,3 +170,19 @@ TEST(VariableReflectionTest, IsStaticVariable) {
   EXPECT_FALSE(Cpp::IsStaticVariable(SubDecls[1]));
   EXPECT_TRUE(Cpp::IsStaticVariable(SubDecls[2]));
 }
+
+TEST(VariableReflectionTest, IsConstVariable) {
+  std::vector<Decl *> Decls, SubDecls;
+  std::string code =  R"(
+    class C {
+      int a;
+      const int b = 2;
+    };
+    )";
+
+  GetAllTopLevelDecls(code, Decls);
+  GetAllSubDecls(Decls[0], SubDecls);
+
+  EXPECT_FALSE(Cpp::IsConstVariable(SubDecls[1]));
+  EXPECT_TRUE(Cpp::IsConstVariable(SubDecls[2]));
+}