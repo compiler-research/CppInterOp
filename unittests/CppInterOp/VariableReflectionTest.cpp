vgvassilev/cling

Implement GetVariableOffset

unittests/libInterOp/VariableReflectionTest.cpp
@@ -56,3 +56,41 @@ TEST(VariableReflectionTest, GetVariableTypeAsString) {
   EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[6]), "E<int>");
   EXPECT_EQ(InterOp::GetVariableTypeAsString(Decls[7]), "E<int> *");
 }
+
+TEST(VariableReflectionTest, GetVariableOffset) {
+  std::vector<Decl *> Decls;
+  std::string code = R"(
+    int a;
+    class C {
+    public:
+      int a;
+      double b;
+      int *c;
+      int d;
+    };
+    )";
+
+  class {
+  public:
+    int a;
+    double b;
+    int *c;
+    int d;
+  } c;
+
+
+  GetAllTopLevelDecls(code, Decls);
+  auto datamembers = InterOp::GetDatamembers(Decls[1]);
+
+  EXPECT_FALSE(InterOp::GetVariableOffset(Interp.get(), Decls[0])
+          == (intptr_t) 0);
+
+  EXPECT_EQ(InterOp::GetVariableOffset(Interp.get(), datamembers[0]),
+          0);
+  EXPECT_EQ(InterOp::GetVariableOffset(Interp.get(), datamembers[1]),
+          ((unsigned long) &(c.b)) - ((unsigned long) &(c.a)));
+  EXPECT_EQ(InterOp::GetVariableOffset(Interp.get(), datamembers[2]),
+          ((unsigned long) &(c.c)) - ((unsigned long) &(c.a)));
+  EXPECT_EQ(InterOp::GetVariableOffset(Interp.get(), datamembers[3]),
+          ((unsigned long) &(c.d)) - ((unsigned long) &(c.a)));
+}

compiler-research/CppInterOp

unittests/CppInterOp/VariableReflectionTest.cpp
@@ -56,3 +56,38 @@ TEST(VariableReflectionTest, GetVariableTypeAsString) {
   EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[6]), "E<int>");
   EXPECT_EQ(Cpp::GetVariableTypeAsString(Decls[7]), "E<int> *");
 }
+
+TEST(VariableReflectionTest, GetVariableOffset) {
+  std::vector<Decl *> Decls;
+  std::string code = R"(
+    int a;
+    class C {
+    public:
+      int a;
+      double b;
+      int *c;
+      int d;
+    };
+    )";
+
+  class {
+  public:
+    int a;
+    double b;
+    int *c;
+    int d;
+  } c;
+
+  GetAllTopLevelDecls(code, Decls);
+  auto datamembers = Cpp::GetDatamembers(Decls[1]);
+
+  EXPECT_FALSE(Cpp::GetVariableOffset(Interp.get(), Decls[0]) == (intptr_t)0);
+
+  EXPECT_EQ(Cpp::GetVariableOffset(Interp.get(), datamembers[0]), 0);
+  EXPECT_EQ(Cpp::GetVariableOffset(Interp.get(), datamembers[1]),
+            ((unsigned long)&(c.b)) - ((unsigned long)&(c.a)));
+  EXPECT_EQ(Cpp::GetVariableOffset(Interp.get(), datamembers[2]),
+            ((unsigned long)&(c.c)) - ((unsigned long)&(c.a)));
+  EXPECT_EQ(Cpp::GetVariableOffset(Interp.get(), datamembers[3]),
+            ((unsigned long)&(c.d)) - ((unsigned long)&(c.a)));
+}