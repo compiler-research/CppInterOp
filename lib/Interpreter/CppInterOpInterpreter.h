959d0e3369f6592de106580084bb29c27217a668
Alexander Penev <7923188+alexander-penev@users.noreply.github.com>
2023-12-22 15:29:23 +0200

    Fix typo in CppInterOpInterpreter.h

--- lib/Interpreter/CppInterOpInterpreter.h
+++ lib/Interpreter/CppInterOpInterpreter.h
@@ -146,7 +146,7 @@ public:
 
     std::vector<const char*> vargs(argv + 1, argv + argc);
     vargs.push_back("-include");
-    vargs.push_back("new")
+    vargs.push_back("new");
     inner = compat::createClangInterpreter(vargs);
   }
 
