7da42f73e56a5423fcb3c98dcec0b5df520637a5
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-08-23 22:31:32 +0100

    Update CppInterOp.cpp

--- lib/Interpreter/CppInterOp.cpp
+++ lib/Interpreter/CppInterOp.cpp
@@ -2632,8 +2632,6 @@ namespace Cpp {
     // FIXME : Workaround for emulated tls symbols not found
     ClingArgv.push_back("-femulated-tls");
     ClingArgv.push_back("-fno-threadsafe-statics");
-    // FIXME: Possible workaround to where JIT cannot find ??_7type_info@@6B@.
-    ClingArgv.push_back("-fno-rtti");
 #endif
     ClingArgv.insert(ClingArgv.end(), Args.begin(), Args.end());
     // To keep the Interpreter creation interface between cling and clang-repl
