72179ab9e64120f572b0da54a0dacc630272d0d3
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-08-24 20:43:28 +0100

    Update CppInterOp.cpp

--- lib/Interpreter/CppInterOp.cpp
+++ lib/Interpreter/CppInterOp.cpp
@@ -2630,7 +2630,8 @@ namespace Cpp {
     // FIXME : Workaround Sema::PushDeclContext assert on windows
     ClingArgv.push_back("-fno-delayed-template-parsing");
     // FIXME : Workaround for emulated tls symbols not found
-    ClingArgv.push_back("-fno-emulated-tls");
+    ClingArgv.push_back("-femulated-tls");
+    ClingArgv.push_back("-fthreadsafe-statics");
 #endif
     ClingArgv.insert(ClingArgv.end(), Args.begin(), Args.end());
     // To keep the Interpreter creation interface between cling and clang-repl
