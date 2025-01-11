90e9c856da0b482db7eede59dfb5b9da58d3f34b
Alexander Penev <alexander.p.penev@gmail.com>
2025-01-11 18:12:34 +0000

    Fix compat in llvm<17 (2)

--- lib/Interpreter/CppInterOp.cpp
+++ lib/Interpreter/CppInterOp.cpp
@@ -3639,7 +3639,7 @@ namespace Cpp {
           if (addr) {
             loadedSymbols[symbol] =
 #if CLANG_VERSION_MAJOR < 17
-              llvm::orc::JITEvaluatedSymbol(addr, JITSymbolFlags::Exported);
+              llvm::JITEvaluatedSymbol(addr, JITSymbolFlags::Exported);
 #else
               llvm::orc::ExecutorSymbolDef(llvm::orc::ExecutorAddr::fromPtr(addr), JITSymbolFlags::Exported);
 #endif // CLANG_VERSION_MAJOR < 17
