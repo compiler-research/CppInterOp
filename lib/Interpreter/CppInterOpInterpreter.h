650d762b3727b4b8ecfa7d3fdeffb00c267ed3bd
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-03-03 17:16:19 +0530

    added undo command for repl

--- lib/Interpreter/CppInterOpInterpreter.h
+++ lib/Interpreter/CppInterOpInterpreter.h
@@ -437,7 +437,7 @@ public:
 #else
     if (llvm::Error Err = Undo(N)) {
       llvm::logAllUnhandledErrors(std::move(Err), llvm::errs(),
-                                  "Failed to undo via ::undo: or something");
+                                  "Failed to undo via ::undo");
       return kFailure;
     }
     return kSuccess;
