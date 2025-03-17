80ceaa4017ba37b4bac21b45f3c6fa9bd1369695
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-03-17 18:27:31 +0530

    skipped undo test for emscripten build

--- unittests/CppInterOp/FunctionReflectionTest.cpp
+++ unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -1617,6 +1617,10 @@ TEST(FunctionReflectionTest, Destruct) {
 }
 
 TEST(FunctionReflectionTest, UndoTest) {
+#ifdef EMSCRIPTEN
+  GTEST_SKIP()
+      << "Currently not implemented while running clang-repl in the broswer";
+#else
   Cpp::CreateInterpreter();
   std::string cerrs;
   testing::internal::CaptureStderr();
@@ -1650,5 +1654,6 @@ TEST(FunctionReflectionTest, UndoTest) {
   EXPECT_EQ(ret, 0);
 #else
   EXPECT_EQ(ret, 1);
+#endif
 #endif
 }
