ac4f7dddcb1defc4216d4a3f5e6f5d8e37b41080
anutosh491 <andersonbhat491@gmail.com>
2025-03-17 12:09:39 +0530

    clang format reviews

--- lib/Interpreter/CppInterOp.cpp
+++ lib/Interpreter/CppInterOp.cpp
@@ -100,7 +100,7 @@ namespace Cpp {
   static clang::ASTContext& getASTContext() { return getSema().getASTContext(); }
 
 #define DEBUG_TYPE "jitcall"
-  CPPINTEROP_API bool JitCall::AreArgumentsValid(void* result, ArgList args,
+  bool JitCall::AreArgumentsValid(void* result, ArgList args,
                                   void* self) const {
     bool Valid = true;
     if (Cpp::IsConstructor(m_FD)) {
@@ -130,7 +130,7 @@ namespace Cpp {
     return Valid;
   }
 
-  CPPINTEROP_API void JitCall::ReportInvokeStart(void* result, ArgList args, void* self) const{
+  void JitCall::ReportInvokeStart(void* result, ArgList args, void* self) const{
     std::string Name;
     llvm::raw_string_ostream OS(Name);
     auto FD = (const FunctionDecl*) m_FD;
@@ -145,7 +145,7 @@ namespace Cpp {
                );
   }
 
-  CPPINTEROP_API void JitCall::ReportInvokeStart(void* object, unsigned long nary,
+  void JitCall::ReportInvokeStart(void* object, unsigned long nary,
                                   int withFree) const {
     std::string Name;
     llvm::raw_string_ostream OS(Name);
