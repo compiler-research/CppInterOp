af7e7ebb3f1de4b375c32b3a2abe84e9e3aa862f
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-04-03 19:56:05 +0100

    Update GoogleTest used to 1.16.0

--- cmake/modules/GoogleTest.cmake
+++ cmake/modules/GoogleTest.cmake
@@ -31,7 +31,7 @@ ExternalProject_Add(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_SHALLOW 1
-  GIT_TAG v1.15.2
+  GIT_TAG v1.16.0
   UPDATE_COMMAND ""
   # # Force separate output paths for debug and release builds to allow easy
   # # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
