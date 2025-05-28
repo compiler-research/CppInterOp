76a9d3e85e37437a237647b2ac31243b485d448c
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-05-28 20:32:42 +0100

    Add initial attempt to build cppyy on Windows

--- include/clang/Interpreter/CppInterOp.h
+++ include/clang/Interpreter/CppInterOp.h
@@ -2,7 +2,12 @@
 #define CLANG_CPPINTEROP_H
 
 #include "CppInterOp/CppInterOp.h"
+#if defined(_MSC_VER)
+#pragma message(                                                               \
+    "#include <clang/Interpreter/CppInterOp.h> is deprecated; use #include <CppInterOp/CppInterOp.h")
+#else
 #warning                                                                       \
     "#include <clang/Interpreter/CppInterOp.h> is deprecated; use #include <CppInterOp/CppInterOp.h"
+#endif
 
 #endif // CLANG_CPPINTEROP_H
