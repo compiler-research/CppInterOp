d6fdbede46b11321cbdf8f418152c7deedd7be49
Vipul Cariappa <vipulcariappa@gmail.com>
2025-06-03 11:13:00 +0200

    reinvent std::pair for test

--- unittests/CppInterOp/FunctionReflectionTest.cpp
+++ unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -1735,41 +1735,75 @@ TEST(FunctionReflectionTest, GetFunctionCallWrapper) {
   EXPECT_EQ(chrono_op_fn_callable.getKind(), Cpp::JitCall::kGenericCall);
 
   Interp->process(R"(
-    #include <tuple>
+    namespace my_std {
+    template <typename T1, typename T2>
+    struct pair {
+        T1 first;
+        T2 second;
+        pair(T1 fst, T2 snd) : first(fst), second(snd) {}
+    };
+
+    template <typename T1, typename T2>
+    pair<T1, T2> make_pair(T1 x, T2 y) {
+        return pair<T1, T2>(x, y);
+    }
+
+    template <int _Int>
+    struct __pair_get;
+
+    template <>
+    struct __pair_get<0> {
+        template <typename T1, typename T2>
+        static constexpr T1 __get(pair<T1, T2> __pair) noexcept {
+            return __pair.first;
+        }
+    };
+
+    template <>
+    struct __pair_get<1> {
+        template <typename T1, typename T2>
+        constexpr T2 __get(pair<T1, T2> __pair) noexcept {
+            return __pair.second;
+        }
+    };
+
+    template <int N, typename T1, typename T2>
+    static constexpr auto get(pair<T1, T2> __t) noexcept {
+        return __pair_get<N>::__get(__t);
+    }
+    }  // namespace my_std
 
     namespace libchemist {
     namespace type {
-        template<typename T> class tensor {};
-    } // namespace type
+    template <typename T>
+    class tensor {};
+    }  // namespace type
 
-    template<typename element_type = double, typename tensor_type = type::tensor<element_type>>
+    template <typename element_type = double,
+              typename tensor_type = type::tensor<element_type>>
     class CanonicalMO {};
 
     template class CanonicalMO<double, type::tensor<double>>;
 
-    auto produce() {
-        return std::make_tuple(10., type::tensor<double>{});
-    }
+    auto produce() { return my_std::make_pair(10., type::tensor<double>{}); }
 
-    } // namespace libchemist
+    }  // namespace libchemist
 
     namespace property_types {
     namespace type {
-        template<typename T>
-        using canonical_mos = libchemist::CanonicalMO<T>;
+    template <typename T>
+    using canonical_mos = libchemist::CanonicalMO<T>;
     }
 
-    auto produce() {
-        return std::make_tuple(5., type::canonical_mos<double>{});
-    }
-    }
+    auto produce() { return my_std::make_pair(5., type::canonical_mos<double>{}); }
+    }  // namespace property_types
 
     auto tmp = property_types::produce();
     auto &p = tmp;
   )");
 
   std::vector<Cpp::TCppFunction_t> unresolved_candidate_methods;
-  Cpp::GetClassTemplatedMethods("get", Cpp::GetScope("std"),
+  Cpp::GetClassTemplatedMethods("get", Cpp::GetScope("my_std"),
                                 unresolved_candidate_methods);
   Cpp::TCppType_t p = Cpp::GetTypeFromScope(Cpp::GetNamed("p"));
   EXPECT_TRUE(p);
