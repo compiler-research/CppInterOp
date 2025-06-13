f2550b85e224a1d14a9b9e29607e3081b353fcde
Aaron Jomy <aaronjomyjoseph@gmail.com>
2025-06-13 11:10:21 +0200

    improve the CppInterOp.h include directory warning

--- include/clang/Interpreter/CppInterOp.h
+++ include/clang/Interpreter/CppInterOp.h
@@ -4,10 +4,10 @@
 #include "CppInterOp/CppInterOp.h"
 #if defined(_MSC_VER)
 #pragma message(                                                               \
-    "#include <clang/Interpreter/CppInterOp.h> is deprecated; use #include <CppInterOp/CppInterOp.h")
+    "#include <clang/Interpreter/CppInterOp.h> is deprecated; use #include <CppInterOp/CppInterOp.h>")
 #else
 #warning                                                                       \
-    "#include <clang/Interpreter/CppInterOp.h> is deprecated; use #include <CppInterOp/CppInterOp.h"
+    "#include <clang/Interpreter/CppInterOp.h> is deprecated; use #include <CppInterOp/CppInterOp.h>"
 #endif
 
 #endif // CLANG_CPPINTEROP_H
