b6fa8735edce291d42eff9ab855b2ead7513e415
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-07-03 13:10:37 +0530

    removed logs from tests

--- unittests/CppInterOp/main.cpp
+++ unittests/CppInterOp/main.cpp
@@ -6,14 +6,12 @@ void parseCustomArguments(int argc, char** argv) {
     std::string arg(argv[i]);
     if (arg == "--use-oop-jit") {
       TestUtils::g_use_oop_jit = true;
-      std::cout << "OOP-JIT mode enabled\n";
     }
   }
 }
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  std::cout << "IN GTESTS" << std::endl;
   parseCustomArguments(argc, argv);
   return RUN_ALL_TESTS();
 }
