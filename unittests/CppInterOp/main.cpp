ebce40a9a62e477b17f8b2c7311b69dc321abd42
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-07-09 18:09:52 +0530

    compiler-rt added in oop build

--- unittests/CppInterOp/main.cpp
+++ unittests/CppInterOp/main.cpp
@@ -1,7 +1,8 @@
 #include "Utils.h"
 #include <gtest/gtest.h>
+#include <string>
 
-void parseCustomArguments(int argc, char** argv) {
+static void parseCustomArguments(int argc, char** argv) {
   for (int i = 1; i < argc; ++i) {
     std::string arg(argv[i]);
     if (arg == "--use-oop-jit") {
