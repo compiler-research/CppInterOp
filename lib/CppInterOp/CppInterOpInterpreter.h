1f43b4ef36b4afae3d383b7b86fa7bcdcc58234c
Vipul Cariappa <vipulcariappa@gmail.com>
2025-10-15 15:12:34 +0200

    Revert Thread-Safety (#729)
    * Revert "Add tests and dev-doc"
    
    This reverts commit c42c90762c5cd839302cd733b103f312c0ae202b.
    
    * Revert "Add `TInterp_t` argument to funcs to resolve interpreter instance"
    
    This reverts commit db363c60d54febf6f7414cca6f7f9119975bad91.
    
    * Revert "Add `Cpp::TakeInterpreter`"
    
    This reverts commit da06567e6ce8bae28dfbf8af610457d8b49fd832.
    
    * Revert "Thread-Safty: Add Mutex per Interpreter"
    
    This reverts commit d210d68f381de9c8e2160fbaaa3495c615b2afce.

--- lib/CppInterOp/CppInterOpInterpreter.h
+++ lib/CppInterOp/CppInterOpInterpreter.h
@@ -140,11 +140,12 @@ namespace Cpp {
 /// CppInterOp Interpreter
 ///
 class Interpreter {
+private:
   std::unique_ptr<clang::Interpreter> inner;
 
-public:
   Interpreter(std::unique_ptr<clang::Interpreter> CI) : inner(std::move(CI)) {}
 
+public:
   static std::unique_ptr<Interpreter>
   create(int argc, const char* const* argv, const char* llvmdir = nullptr,
          const std::vector<std::shared_ptr<clang::ModuleFileExtension>>&
