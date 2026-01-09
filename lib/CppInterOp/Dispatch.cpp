94b5d94a2730d2676a4d3cefec15bce8b9132807
Aaron Jomy <aaronjomyjoseph@gmail.com>
2026-01-09 17:42:57 +0100

    Review comments, code optimizations and other improvements

--- lib/CppInterOp/Dispatch.cpp
+++ lib/CppInterOp/Dispatch.cpp
@@ -1,25 +1,18 @@
-//------------------------------------------------------------------------------
-// CppInterOp Dispatch Implementation
-// author:  Aaron Jomy <aaron.jomy@cern.ch>
-//------------------------------------------------------------------------------
-
 #include <CppInterOp/Dispatch.h>
 
+#include <string>
 #include <unordered_map>
 
-static const std::unordered_map<std::string_view, __CPP_FUNC>
-    INTEROP_FUNCTIONS = {
-#define X(name, type) {#name, (__CPP_FUNC) static_cast<type>(&CppImpl::name)},
-        CPPINTEROP_API_MAP
-#undef X
+static const std::unordered_map<std::string_view, CppFnPtrTy>
+    DispatchMap = {
+#define DISPATCH_API(name, type) {#name, (CppFnPtrTy) static_cast<type>(&CppImpl::name)},
+        CPPINTEROP_API_TABLE
+#undef DISPATCH_API
 };
 
-#undef MAP_ENTRY_SIMPLE
-#undef MAP_ENTRY_OVERLOADED
-
-static inline __CPP_FUNC _cppinterop_get_proc_address(const char* funcName) {
-  auto it = INTEROP_FUNCTIONS.find(funcName);
-  return (it != INTEROP_FUNCTIONS.end()) ? it->second : nullptr;
+static inline CppFnPtrTy _cppinterop_get_proc_address(const char* funcName) {
+  auto it = DispatchMap.find(funcName);
+  return (it != DispatchMap.end()) ? it->second : nullptr;
 }
 
 void (*CppGetProcAddress(const unsigned char* procName))(void) {
