d7934219109ddcef2d817d28917621183c794c7a
Aaron Jomy <aaronjomyjoseph@gmail.com>
2026-01-17 11:40:00 +0100

    more clang-tidy

--- include/CppInterOp/Dispatch.h
+++ include/CppInterOp/Dispatch.h
@@ -193,7 +193,7 @@ inline void* dlGetProcAddress(const char* name,
 #else
     void* handle = dlopen(path, RTLD_LOCAL | RTLD_NOW);
     if (handle) {
-      //NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
       getProc = reinterpret_cast<void* (*)(const char*)>(
           dlsym(handle, "CppGetProcAddress"));
       if (!getProc) dlclose(handle);
@@ -207,13 +207,14 @@ inline void* dlGetProcAddress(const char* name,
 // CppAPIType is used for the extern clauses below
 // FIXME: drop the using clauses
 namespace CppAPIType {
+// NOLINTBEGIN(bugprone-macro-parentheses)
 #define DISPATCH_API(name, type) using name = type;
 CPPINTEROP_API_TABLE
 #undef DISPATCH_API
+// NOLINTEND(bugprone-macro-parentheses)
 } // end namespace CppAPIType
 
-namespace CppInternal {
-namespace Dispatch {
+namespace CppInternal::Dispatch {
 
 // FIXME: This is required for the types, but we should move the types
 // into a separate namespace and only use that scope (CppImpl::Types)
@@ -262,8 +263,7 @@ inline void UnloadDispatchAPI() {
     CPPINTEROP_API_TABLE
 #undef DISPATCH_API
 }
-} // namespace Dispatch
-} // namespace CppInternal
+} // namespace CppInternal::Dispatch
 
 namespace Cpp = CppInternal::Dispatch;
 #endif // CPPINTEROP_DISPATCH_H
--- lib/CppInterOp/Dispatch.cpp
+++ lib/CppInterOp/Dispatch.cpp
@@ -3,12 +3,14 @@
 #include <string_view>
 #include <unordered_map>
 
+// NOLINTBEGIN(cppcoreguidelines-pro-type-cstyle-cast)
 static const std::unordered_map<std::string_view, CppFnPtrTy>
     DispatchMap = {
 #define DISPATCH_API(name, type) {#name, (CppFnPtrTy) static_cast<type>(&CppImpl::name)},
         CPPINTEROP_API_TABLE
 #undef DISPATCH_API
 };
+// NOLINTEND(cppcoreguidelines-pro-type-cstyle-cast)
 
 CppFnPtrTy CppGetProcAddress(const char* funcName) {
   auto it = DispatchMap.find(funcName);
