0022d04ce0717b25b79309c617df2ae848cb1433
Vipul Cariappa <vipulcariappa@gmail.com>
2026-01-30 12:51:28 +0100

    prioritize resource-dir provided by user over auto-detecting

--- lib/CppInterOp/CppInterOp.cpp
+++ lib/CppInterOp/CppInterOp.cpp
@@ -3310,12 +3310,30 @@ void AddLibrarySearchPaths(const std::string& ResourceDir,
                                                  false, false);
   }
 }
+std::string ExtractArgument(const std::vector<const char*>& Args,
+                            const std::string& Arg) {
+  size_t I = 0;
+  for (auto i = Args.begin(); i != Args.end(); i++) {
+    if ((++I < Args.size()) && (*i == Arg)) {
+      return *(++i);
+    }
+  }
+  return "";
+}
 } // namespace
 
 TInterp_t CreateInterpreter(const std::vector<const char*>& Args /*={}*/,
                             const std::vector<const char*>& GpuArgs /*={}*/) {
   std::string MainExecutableName = sys::fs::getMainExecutable(nullptr, nullptr);
-  std::string ResourceDir = MakeResourcesPath();
+  // In some systems, CppInterOp cannot manually detect the correct resource.
+  // Then the -resource-dir passed by the user is assumed to be the correct
+  // location. Prioritising it over detecting it within CppInterOp. Extracting
+  // the resource-dir from the arguments is required because we set the
+  // necessary library search location explicitly below. Because by default,
+  // linker flags are ignored in repl (issue #748)
+  std::string ResourceDir = ExtractArgument(Args, "-resource-dir");
+  if (ResourceDir.empty())
+    ResourceDir = MakeResourcesPath();
   llvm::Triple T(llvm::sys::getProcessTriple());
   namespace fs = std::filesystem;
   if ((!fs::is_directory(ResourceDir)) && (T.isOSDarwin() || T.isOSLinux()))
