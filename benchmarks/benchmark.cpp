d561282737a2a9b6e3a800ffe6fd86626bbcc1fc
mcbarton <150042563+mcbarton@users.noreply.github.com>
2026-03-25 19:08:47 +0000

    Try to enable benchmarks for native case

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -1,6 +1,9 @@
 #include "CppInterOp/CppInterOp.h"
+#include <chrono>
 #include <cstdio>
+#ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#endif
 #include <functional>
 #include <string>
 #include <vector>
@@ -12,15 +15,26 @@ struct Benchmark {
    std::function<void()> func;
 };
 
+static double now_ms() {
+#ifdef __EMSCRIPTEN__
+    return emscripten_get_now();
+#else
+    using namespace std::chrono;
+    return duration<double, std::milli>(
+        high_resolution_clock::now().time_since_epoch()
+    ).count();
+#endif
+}
+
 static void run_benchmarks(const std::vector<Benchmark>& benchmarks) {
    for (const auto& bench : benchmarks) {
        bench.func();
 
-       double start = emscripten_get_now();
+       double start = now_ms();
        for (int i = 0; i < iterations; ++i) {
            bench.func();
        }
-       double end = emscripten_get_now();
+       double end = now_ms();
        double elapsed_ms = end - start;
        double avg_time = elapsed_ms / iterations;
 
@@ -34,7 +48,11 @@ static void cpp_getversion() {
 }
 
 static void cpp_process() {
-  Cpp::CreateInterpreter();
+  static bool initialized = false;
+  if (!initialized) {
+    Cpp::CreateInterpreter();
+    initialized = true;
+  }
   Cpp::Process("int a = 12;");
 }
 
